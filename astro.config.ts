8dfa55728f0c7d75bb09a1596ed7292b09b8feb1
DavidMB4 <david.maldonado.4810@gmail.com>
2025-11-04 22:41:33 -0600

    avance pagina de noticias y eventos

--- astro.config.ts
+++ astro.config.ts
@@ -5,14 +5,14 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import Auth from "auth-astro";
-import vercel from "@astrojs/react"
+import vercel from '@astrojs/vercel/serverless'
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
   integrations: [react(), markdoc(), keystatic(), Auth()],
 
   vite: {
