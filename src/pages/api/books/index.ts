7c7e34482f4599a3a13e4b9abe858b65061c74fe
TheMangito <themanguito21@gmail.com>
2025-12-15 22:54:06 -0600

    Cambios en el diseño para hacer responsive el header

--- src/pages/api/books/index.ts
+++ src/pages/api/books/index.ts
@@ -1,19 +1,34 @@
 import type { APIRoute } from "astro";
-import prisma from "../../../lib/prisma"; 
+import prisma from "../../../lib/prisma"; // Ajusta la ruta según tu estructura
 
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
+  const query = url.searchParams.get("q");
+
   try {
+    const whereClause = query
+      ? {
+          OR: [
+            { title: { contains: query } },
+          ],
+        }
+      : {};
+
     const books = await prisma.book.findMany({
+      where: whereClause,
       include: { author: true },
+      take: 5, 
     });
 
     return new Response(JSON.stringify(books), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
-
   } catch (error) {
     console.error("Error al obtener libros:", error);
-    return new Response(JSON.stringify({ error: "Error al leer la base de datos" }), { status: 500 });
+    return new Response(
+      JSON.stringify({ error: "Error al leer la base de datos" }),
+      { status: 500 }
+    );
   }
 };
